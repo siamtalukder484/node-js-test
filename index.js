419e2df169490de0314f28be97096226b7d94111
Shohelrana63 <shohelrana.beg@gmail.com>
2023-03-21 22:19:55 +0600

    nodemon pblm solve

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const express = require("express")
-const app = express()
+const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
 
-const check = require("./middleware/checker")
-app.use(express.json())
+const check = require("./middleware/checker");
+app.use(express.json());
 
+mongoose
+  .connect(
+    "mongodb+srv://siamcit:siamcit@siam-dev.zyltwrw.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected!"));
 
-mongoose.connect('mongodb+srv://siamcitbd:siamcitbd@siam-dev.zyltwrw.mongodb.net/test?retryWrites=true&w=majority')
-  .then(() => console.log('Connected!'));
+app.get("/", check, function (req, res) {
+  let user = [
+    {
+      name: "siam",
+    },
+    {
+      name: "siam2",
+    },
+    {
+      name: "siam3",
+    },
+    {
+      name: "siam4",
+    },
+  ];
+  res.json(user);
+});
 
+app.post("/reg", (req, res) => {
+  let { name, email } = req.body;
+  if (name == "") {
+    return res.json({ error: "Name is required" });
+  } else {
+    res.json({ success: "Form is validate" });
+  }
+  console.log(req.body);
+});
 
-app.get("/",check,function(req,res){
-    let user = [
-        {
-            name:"siam",
-        },
-        {
-            name:"siam2",
-        },
-        {
-            name:"siam3",
-        },
-        {
-            name:"siam4",
-        }
-    ]
-    res.json(user)
-})
-
-app.post("/reg",(req,res)=>{
-    let {name, email} = req.body
-    if(name == ""){
-        return res.json({error: "Name is required"})
-    }else{
-        res.json({success: "Form is validate"})
-    }
-    console.log(req.body)
-})
-
-app.listen(8000,function(){
-    console.log("port running");
-})
+app.listen(8000, function () {
+  console.log("port running");
+});
